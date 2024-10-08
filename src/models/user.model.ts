9d3840deefaf8c146e4e4214444acbed289ab1bc
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2024-10-08 09:12:21 +0530

    added

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,7 +8,7 @@ interface IUser {
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true },
-}, {versionKey: false});
+}, {versionKey: false}); // versionKey: flase using do not allow to store __v field in mongodb for each records.
 
 const User = model<IUser>('User', userSchema);
 
